be938c89dca268ac927615d24e9aef05d7b4529a
pbradford0 <phil.w.bradford@gmail.com>
2017-01-07 19:39:43 -0500

    initalize

---
+++ day15.py
@@ -0,0 +1,18 @@
+#Author: Phil Bradford
+#Solution for http://adventofcode.com/2015/day/15
+
+import sys
+
+def main():
+  if len(sys.argv) != 2:
+    print 'Please specify an input file'
+    sys.exit(1)
+
+  #a = race(sys.argv[1])
+  #print "Best reindeer's distance: " + str(a)
+  
+  #b = race_pts(sys.argv[1])
+  #print "Pointiest reindeer: " + str(b)
+
+if __name__ == '__main__':
+  main()
